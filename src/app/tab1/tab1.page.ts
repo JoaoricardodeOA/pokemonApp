7b93facc8a2eca0cf33ba5ec144e62825026ceea
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2023-11-04 21:49:43 -0300

    creation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PokeApiService } from '../services/poke-api.service';
+import { ViaCEPService } from '../services/via-cep.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  areaBuscarPokemon:string='52011210';
+  areaBusca:any={
+    bairro : '',
+    localidade : '',
+    logradouro : '',
+    uf:''
+  };
 
-  constructor() {}
 
+  constructor(
+    private pokeApiService:PokeApiService,
+    private viaCEPService:ViaCEPService
+  ){}
+
+  buscarPokemon(){
+    this.viaCEPService.getViaCEPService(this.areaBuscarPokemon)
+      .subscribe((value)=>{
+        this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
+        this.areaBusca.bairro = ', '+JSON.parse(JSON.stringify(value))['bairro'];
+        this.areaBusca.localidade = ' - '+JSON.parse(JSON.stringify(value))['localidade'];
+        this.areaBusca.uf = '-'+JSON.parse(JSON.stringify(value))['uf'];
+      });
+    this.pokeApiService.getPokeApiService();
+  }
 }
