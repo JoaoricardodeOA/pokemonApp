d4f41f32f0e12f3d2b403d35ca53075bde0d8e6f
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2023-11-12 18:08:16 -0300

    Added some changes on methods of requisition to fix bugs on services (not totally fixed)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PokeApiService } from '../services/poke-api.service';
 import { ViaCEPService } from '../services/via-cep.service';
 
@@ -7,7 +7,8 @@ import { ViaCEPService } from '../services/via-cep.service';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+
+export class Tab1Page implements OnInit{
   areaBuscarPokemon:string='52011210';
   areaBusca:any={
     bairro : '',
@@ -15,7 +16,7 @@ export class Tab1Page {
     logradouro : '',
     uf:''
   };
-  pokemon:any={
+  public pokemon:any={
     name:'bulbasaur',
     image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg',
     abilities:'2',
@@ -29,22 +30,33 @@ export class Tab1Page {
     private pokeApiService:PokeApiService,
     private viaCEPService:ViaCEPService
   ){}
+  ngOnInit(): void {
+    this.buscarPokemon(this.areaBuscarPokemon)
+  }
 
-  buscarPokemon(areaBuscarPokemon:string){
+  
+  async buscarPokemon(areaBuscarPokemon:string){
     this.viaCEPService.getViaCEPService(areaBuscarPokemon)
       .subscribe((value)=>{
         this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
         this.areaBusca.bairro = ', '+JSON.parse(JSON.stringify(value))['bairro'];
         this.areaBusca.localidade = ' - '+JSON.parse(JSON.stringify(value))['localidade'];
         this.areaBusca.uf = '-'+JSON.parse(JSON.stringify(value))['uf'];
-      });
-    this.pokeApiService.getPokeApiService()
-      .subscribe((value)=>{
+      }); 
+      let service =  await this.pokeApiService.getPokeApiService()
+      service
+      .subscribe(value=>{
         this.pokemon.weight = JSON.parse(JSON.stringify(value))['weight'];
         this.pokemon.name = JSON.parse(JSON.stringify(value))['name'];
         this.pokemon.height = JSON.parse(JSON.stringify(value))['height'];
         this.pokemon.abilities = JSON.parse(JSON.stringify(value))['abilities'].length;
         this.pokemon.image = JSON.parse(JSON.stringify(value))['sprites'].other.dream_world.front_default;
+        this.pokeApiService.lastPokemonAbility = JSON.parse(JSON.stringify(value))['abilities'].length;
+        console.log(JSON.parse(JSON.stringify(value))['abilities'].length)
       });
+      console.log(this.pokeApiService.lastPokemonAbility)
+      this.pokeApiService.lastPokemonAbility = this.pokemon.abilities
+      
+      console.log(this.pokeApiService.lastPokemonAbility)
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,42 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PokeApiService } from '../services/poke-api.service';
 import { PhotoService } from '../services/photo.service';
+import { Tab1Page } from '../tab1/tab1.page';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-  pokemon:any={
-    name:'bulbasaur',
-    image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg',
-    abilities:'2',
-    height:'7',
-    weight:'69'
+export class Tab2Page implements OnInit{
+  pokemonAdversary:any={
+    name:'',
+    image:'',
+    abilities:'',
+    height:'',
+    weight:''
   
   }
-
+  resultado:string = '' 
+  
 
   constructor(
     private pokeApiService:PokeApiService,
     public photoService:PhotoService
   ){
+    
+  }
+  ngOnInit(): void {
     this.buscarPokemon()
+    this.setResultado()
   }
 
-  buscarPokemon(){
-    this.pokeApiService.getPokeApiService()
-      .subscribe((value)=>{
-        this.pokemon.weight = JSON.parse(JSON.stringify(value))['weight'];
-        this.pokemon.name = JSON.parse(JSON.stringify(value))['name'];
-        this.pokemon.height = JSON.parse(JSON.stringify(value))['height'];
-        this.pokemon.abilities = JSON.parse(JSON.stringify(value))['abilities'].length;
-        this.pokemon.image = JSON.parse(JSON.stringify(value))['sprites'].other.dream_world.front_default;
+  async buscarPokemon(){
+    let service =  await this.pokeApiService.getPokeApiService()
+     service
+      .subscribe(value =>{
+        this.pokemonAdversary.weight = JSON.parse(JSON.stringify(value))['weight'];
+        this.pokemonAdversary.name = JSON.parse(JSON.stringify(value))['name'];
+        this.pokemonAdversary.height = JSON.parse(JSON.stringify(value))['height'];
+        this.pokemonAdversary.abilities = JSON.parse(JSON.stringify(value))['abilities'].length;
+        this.pokemonAdversary.image = JSON.parse(JSON.stringify(value))['sprites'].other.dream_world.front_default;
+        this.pokeApiService.adversaryAbility = JSON.parse(JSON.stringify(value))['abilities'].length;  
+        console.log(JSON.parse(JSON.stringify(value))['abilities'].length)
       });
+      console.log(this.pokeApiService.lastPokemonAbility)
+      this.pokeApiService.lastPokemonAbility = this.pokemonAdversary.abilities
+      
+      console.log(this.pokeApiService.lastPokemonAbility)
   }
   addPhotoToGallery(){
     this.photoService.addNewToGallery()
+  }
+  setResultado(){
+    console.log(this.pokeApiService.lastPokemonAbility)
+    console.log(this.pokeApiService.adversaryAbility)
+     if(this.pokeApiService.adversaryAbility === this.pokeApiService.lastPokemonAbility){
+        this.resultado = 'empatou'
+     }else if(this.pokeApiService.adversaryAbility > this.pokeApiService.lastPokemonAbility){
+        this.resultado = 'ganhou'
+     }else{
+      this.resultado = 'perdeu'
+     }
   }
 
 }
